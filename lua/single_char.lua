925dfe0af6b9e2ba82a8ea61f576b149db181109
luozikuan <luozikuan@yeah.net>
2026-06-10 14:12:31 +0800

    single_char.lua: no need to check English candidate, allow multi char for mkst words
    Signed-off-by: luozikuan <luozikuan@yeah.net>

--- lua/single_char.lua
+++ lua/single_char.lua
@@ -3,8 +3,7 @@ function single_char(input, env)
 	local single_option = env.engine.context:get_option("single_char")
 	local input_text = env.engine.context.input
 	for cand in input:iter() do
-        local is_all_english = cand.text:match("^[A-Za-z]+$") ~= nil
-		if (not single_option or utf8.len(cand.text) == 1 or input_text:find("^`") or is_all_english) then
+		if (not single_option or utf8.len(cand.text) == 1 or input_text:find("`")) then
 			yield(cand)
 		end
 	end
