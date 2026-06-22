4fde437c93474b8d43abbee626a8a7ee5c83832a
luozikuan <luozikuan@yeah.net>
2026-06-22 17:09:50 +0800

    lua: sing_char don't filter …… and ——
    Signed-off-by: luozikuan <luozikuan@yeah.net>

--- lua/single_char.lua
+++ lua/single_char.lua
@@ -3,7 +3,9 @@ function single_char(input, env)
 	local single_option = env.engine.context:get_option("single_char")
 	local input_text = env.engine.context.input
 	for cand in input:iter() do
-		if (not single_option or utf8.len(cand.text) == 1 or input_text:find("`")) then
+		if (not single_option or utf8.len(cand.text) == 1 or input_text:find("`")
+		    or cand.text == "……" or cand.text == "——")
+		then
 			yield(cand)
 		end
 	end
